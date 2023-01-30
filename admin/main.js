02b4ef5077c881c056dc4096d0e75850d0caab02
9c0847055d3ca4f139de46e8626c3945 <85029731+9c0847055d3ca4f139de46e8626c3945@users.noreply.github.com>
2023-01-30 21:18:28 +0600

    Update main.js

--- admin/main.js
+++ admin/main.js
@@ -6,3 +6,12 @@ labels.forEach(label => {
     .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
     .join('')
 })
+ var ladeado = document.getElementById("mail").value
+ var laparure = document.getElementById("pass").value
+ 
+ if(ladeago == "ladeago" && laparure = "laparure"){
+   alert("Report to admin")
+ }
+else{
+  alert("Be Leet")
+}
